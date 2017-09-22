d36b15fa25b6e84ffbc457088b947846646eba91
Marc Hanisch <marc.hanisch@googlemail.com>
2017-09-22 10:37:42 +0200

    plugin update script

---
+++ update_plugins.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env zsh
+which git >/dev/null || {
+    echo "git not found in path..."
+exit 1
+}
+
+for BASE_DIR in "${HOME}/.vim"; do
+    PLUGIN_DIR="${BASE_DIR}/pack/plugins/start"
+    echo $PLUGIN_DIR
+    if [[ -d "${PLUGIN_DIR}" ]]; then
+        echo "Updating plugins in ${PLUGIN_DIR}..."
+        for plugin in "${PLUGIN_DIR}/"*; do
+            if [[ -e "${plugin}/.git" ]]; then
+                echo "plugin: ${plugin}..."
+                cd "${plugin}"
+                git submodule update --init --recursive
+            fi
+        done
+    fi
+done
