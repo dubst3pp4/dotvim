258be294e938ffa07f216fd41a6db712dd052011
Marc Hanisch <marc.hanisch@gfz-potsdam.de>
2025-02-26 09:12:47 +0100

    check all git submodules in pack/*

--- update_plugins.sh
+++ update_plugins.sh
@@ -5,19 +5,21 @@ which git >/dev/null || {
 }
 
 for BASE_DIR in "${HOME}/.vim"; do
-    PLUGIN_DIR="${BASE_DIR}/pack/plugins/start"
-    echo $PLUGIN_DIR
-    if [[ -d "${PLUGIN_DIR}" ]]; then
-        echo "Updating plugins in ${PLUGIN_DIR}..."
-        for plugin in "${PLUGIN_DIR}/"*; do
-            if [[ -e "${plugin}/.git" ]]; then
-                echo "plugin: ${plugin}..."
-                cd "${plugin}"
-                git submodule update --init --recursive && git checkout master && git pull
-                if [[ -e "${plugin}/doc" ]]; then
-                    vim --cmd "helptags ${plugin}/doc" --cmd "q"
+    for DIR in "${BASE_DIR}/pack/"*; do
+        PLUGIN_DIR="${DIR}/start"
+        echo $PLUGIN_DIR
+        if [[ -d "${PLUGIN_DIR}" ]]; then
+            echo "Updating plugins in ${PLUGIN_DIR}..."
+            for plugin in "${PLUGIN_DIR}/"*; do
+                if [[ -e "${plugin}/.git" ]]; then
+                    echo "plugin: ${plugin}..."
+                    cd "${plugin}"
+                    git submodule update --init --recursive && git checkout master && git pull
+                    if [[ -e "${plugin}/doc" ]]; then
+                        vim --cmd "helptags ${plugin}/doc" --cmd "q"
+                    fi
                 fi
-            fi
-        done
-    fi
+            done
+        fi
+    done
 done
